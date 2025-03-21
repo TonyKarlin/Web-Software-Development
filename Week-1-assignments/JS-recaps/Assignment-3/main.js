0eb66a2b73562c73bd15ce0f7e8f34c20f328e65
Tony Karlin <156673907+TonyKarlin@users.noreply.github.com>
2025-03-22 01:53:40 +0200

    Initial commit

---
+++ Week-1-assignments/JS-recaps/Assignment-3/main.js
@@ -0,0 +1,18 @@
+const a = prompt('Enter length of side a: ');
+const b = prompt('Enter length of side b: ');
+const c = prompt('Enter length of side c: ');
+
+function triangleType(a, b, c) {
+  if (a === b && b === c) {
+    return 'Equilateral';
+  } else if (a === b || b === c || a === c) {
+    return 'Isosceles';
+  } else {
+    return 'Scalene';
+  }
+}
+
+if (a !== null && b !== null && c !== null) {
+  document.getElementById('result').innerHTML =
+    'Triangle type: ' + triangleType(a, b, c);
+}
